967a50a299ce821770b10eba672960cfe6747bad
Christopher Kenny <ctk44@cornell.edu>
2021-08-20 23:39:54 -0400

    minichusetts minissouri

--- R/toy/00_toy_setup.R
+++ R/toy/00_toy_setup.R
@@ -1,8 +1,33 @@
 # make grid ----
 bb <- st_sfc(st_polygon(list(rbind(c(0,0), c(1,0), c(1,1), c(0,0)))))
-grid <- st_as_sf(st_make_grid(bb, n = 6))
+grid <- st_make_grid(bb, n = 6)
+tb <- st_as_sf(grid)
+
 
 # add votes ----
+minichusetts <- tb %>%
+    mutate(row = rep(6:1, each=6),
+           col = rep(1:6, 6),
+           pop = 5,
+           dem = 3,
+           rep = 2
+           )
+
+minissouri <- tb %>%
+    mutate(row = rep(6:1, each=6),
+           col = rep(1:6, 6),
+           city1 = row <= 2 & col >= 5,
+           city2 = row >= 5 & col <= 2,
+           pop = 4,
+           pop = if_else(city1, 10, pop),
+           pop = if_else(city2, 7, pop),
+           dem = 1,
+           dem = if_else(city1, 8, dem),
+           dem = if_else(city2, 5, dem),
+           rep = 3,
+           rep = if_else(city1, 2, rep),
+           rep = if_else(city2, 2, rep)
+           )
 
 mininois = grid %>%
     mutate(row = rep(6:1, each=6),
