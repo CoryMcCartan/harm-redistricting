6438b3fde2d2386dc87b1b61e1548a54e826ed0f
Cory McCartan <cory.mccartan@gmail.com>
2021-08-20 23:29:36 -0400

    mininois update

--- R/toy/00_toy_setup.R
+++ R/toy/00_toy_setup.R
@@ -1,6 +1,7 @@
 # make grid ----
 bb <- st_sfc(st_polygon(list(rbind(c(0,0), c(1,0), c(1,1), c(0,0)))))
-grid <- st_as_sf(st_make_grid(bb, n = 6))
+grid <- st_as_sf(st_make_grid(bb, n = 6, crs = 3857)) %>%
+    rename(geometry=x)
 
 # add votes ----
 
@@ -10,4 +11,6 @@ mininois = grid %>%
            city = row <= 3 & col <= 3,
            pop = if_else(city, 11, 3),
            dem = if_else(city, 8, 1),
-           rep = if_else(city, 3, 2))
+           rep = if_else(city, 3, 2)) %>%
+    relocate(geometry, .after=rep) %>%
+    redist_map(ndists=3, pop_tol=0.1)
