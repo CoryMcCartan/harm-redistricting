fa51c291310687987651a2a1ff9508cc22038805
Cory McCartan <cory.mccartan@gmail.com>
2021-08-20 23:52:36 -0400

    start metrics

---
+++ R/01_metrics.R
@@ -0,0 +1,10 @@
+#' Democratic share of district
+#'
+#' Returns a mtrix of precincts by plans, where each entry is the democratic
+#' share in the district the precicnt belongs to in that plan.
+#'
+#' @param plans a `redist_plans` object
+#'
+#' @returns a matrix
+district_dem = function(plans) {
+}
