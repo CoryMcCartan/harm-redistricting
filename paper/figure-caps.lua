4d58c7cf84866faa72d5e6e03634f5b6a05b78e7
Christopher Kenny <ctk44@cornell.edu>
2026-03-06 15:37:55 -0500

    copy captions

---
+++ paper/figure-caps.lua
@@ -0,0 +1,36 @@
+-- Collects all figure captions and inserts copies at the #figure-caps div.
+
+local figure_captions = {}
+local fig_count = 0
+local in_appendix = false
+
+return {
+  -- First pass: collect captions, stopping when the appendix begins
+  {
+    Header = function(el)
+      if el.classes:includes("appendix") then
+        in_appendix = true
+      end
+    end,
+    FloatRefTarget = function(el)
+      if in_appendix then return end
+      if el.type == "Figure" and el.caption_long then
+        fig_count = fig_count + 1
+        local prefix = pandoc.List({
+          pandoc.Strong({pandoc.Str("Figure " .. fig_count .. ".")}),
+          pandoc.Space()
+        })
+        local inlines = prefix .. pandoc.List(el.caption_long.content)
+        table.insert(figure_captions, pandoc.Para(inlines))
+      end
+    end
+  },
+  -- Second pass: replace #figure-caps div with the collected captions
+  {
+    Div = function(div)
+      if div.identifier == "figure-caps" then
+        return pandoc.Div(figure_captions, div.attr)
+      end
+    end
+  }
+}
